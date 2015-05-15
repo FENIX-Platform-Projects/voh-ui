7b9df914d178c68a96723d0ddc3d5bb820662fec
simonefenix <simone.murzilli@fao.org>
2015-05-15 14:34:43 +0200

    added layer label

--- i18n/en/home.js
+++ i18n/en/home.js
@@ -11,7 +11,8 @@ define([
         "database_update_title": "Database Updates",
         "documents_links_title": "Documents",
         "home_intro_title": "Welcome to Voices of the Hungry Data Portal",
-        "home_intro_text": "Voices of the Hungry is an initiative by FAO with financial support of United Kingdom and Belgium. The project is seeking resources partners to extend the data collection beyond 2015 and to strengthen the capacity development activities. To know more about the project and get in touch with the VOH team, please visit the VOH Home."
+        "home_intro_text": "Voices of the Hungry is an initiative by FAO with financial support of United Kingdom and Belgium. The project is seeking resources partners to extend the data collection beyond 2015 and to strengthen the capacity development activities. To know more about the project and get in touch with the VOH team, please visit the VOH Home.",
+        "food_insecurity": "Food Insecurity"
     });
 
 });
