95f20335707b70289570464aeb3a8b5e75ffea2a
danielefenix <daniele.salvatore@fao.org>
2015-05-18 15:24:25 +0200

    Update Region Query for chart and olap

--- config/Services.js
+++ config/Services.js
@@ -6,10 +6,10 @@ define(function ( ) {
     return {
 
         CHART_COUNTRY: "SELECT country as geo, country_label as geo_label, variable, group_code, ms, s FROM master_aggregation JOIN codes_country on (country = country_code) WHERE country IN ( '{query_geo}' ) AND variable IN ( '{query_variables}' ) ORDER BY variable, country, group_code",
-        CHART_REGION: "SELECT geo, geo_label, variable, group_code, ms, s FROM master_aggregation_region JOIN codes_country on (country = country_code) WHERE country IN ( '{query_geo}' ) AND variable IN ( '{query_variables}' ) ORDER BY variable, country, group_code",
+        CHART_REGION: "SELECT region as geo, region_label as geo_label, variable, group_code, ms, s FROM master_aggregation_region JOIN codes_region on (region = region_code) WHERE region IN ( '{query_geo}' ) AND variable IN ( '{query_variables}' ) ORDER BY variable, region, group_code",
 
         OLAP_COUNTRY: "SELECT country as geo, country_label as geo_label, variable, group_code, ms, s FROM master_aggregation JOIN codes_country on (country = country_code) WHERE country IN ( '{query_geo}' ) AND variable IN ( '{query_variables}' ) ORDER BY variable, country, group_code",
-        OLAP_REGION: "SELECT geo, geo_label, variable, group_code, ms, s FROM master_aggregation_region JOIN codes_country on (country = country_code) WHERE country IN ( '{query_geo}' ) AND variable IN ( '{query_variables}' ) ORDER BY variable, country, group_code",
+        OLAP_REGION: "SELECT region as geo, region_label as geo_label, variable, group_code, ms, s FROM master_aggregation_region JOIN codes_region on (region = region_code) WHERE region IN ( '{query_geo}' ) AND variable IN ( '{query_variables}' ) ORDER BY variable, region, group_code",
 
         CL_MARITAL: "SELECT marital_code as code, marital_label FROM codes_marital;",
         CL_AGE : "SELECT age_range_code as code, age_range_label FROM codes_age_range;",
